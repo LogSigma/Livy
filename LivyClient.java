33a8e4f818f46895d8153cac2f4aae454f868d56
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2018-03-26 10:31:17 +0900

    Create LivyClient.java

---
+++ LivyClient.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+pulbic class LivyClient{
+  static LivyClientManager livyClientManager = new LivyClientManager();
+  
+  public static void main(String{} args) throws Exception {
+    try{
+      //job
+      livyClientJobData ClientJobData = livyClientManager.ExuteJob();
+      
+      boolean success -false;
+      while (true){
+        String status = livyClientManager.GetJobStatus(clientJobData.id);
+        
+        if(status.equals("success")){
+          success = true;
+          break;
+        } else if(status.equals("starting") || status.equals("running")) {
+          Thread.sleep(1000L);
+          continue;
+        } ekse {
+          success = false;
+          break;
+        }
+      }
+      
+      if(success){
+        String resultData = livyClientManager.GetResult();
+        livyClientManager.ClearResult();
+      }
+    }cathe(IOException e){
+      e.printStackTrace();
+    }
+  }
+  
+}
